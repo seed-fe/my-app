bd63da4295a39612af6be93028a5fd3f325e6777
Bill Zhang <zzlfullstack@163.com>
2019-08-23 18:03:10 +0800

    实现了完整的井字棋

--- src/index.js
+++ src/index.js
@@ -22,6 +22,10 @@ class Board extends React.Component {
   handleClick(i) {
     // slice无参数可实现浅复制，浅复制拷贝的是引用，两个变量指向同一个数组
     const squares = this.state.squares.slice();
+    // 当有玩家胜出或某个square已被填充，该函数不做任何处理直接返回
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -38,7 +42,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -85,4 +95,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
-  
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}  
