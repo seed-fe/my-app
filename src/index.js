bc59a1f4e8fdd424d3cb6038325b06de405dd180
Bill Zhang <zzlfullstack@163.com>
2019-08-23 15:28:01 +0800

    完成了落子功能

--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
+    constructor(props) {
+      // 当子类中有构造函数时必须调用super方法，super是父类的构造函数，用来新建父类的this对象，因为子类自己的this对象必须先通过父类的构造函数完成塑造，得到与父类同样的实例属性和方法。
+      // 仅当想在constructor内使用props才将props传入super，将props传入super的作用是可以使你在constructor内访问它。
+      super(props);
+      this.state = {
+        value: null,
+      };
+    }
     render() {
       return (
-        <button className="square" onClick={function() { alert('click'); }}>
-          {this.props.value}
+        <button 
+          className="square" 
+          onClick={() => this.setState({value: 'X'})}
+        >
+          {/* 这里用了this但没有调用super，仅当存在constructor的时候必须调用super，如果没有，则不用 */}
+          {this.state.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    // 仅当存在constructor的时候必须调用super，如果没有，则不用，参考https://segmentfault.com/a/1190000008165717
     renderSquare(i) {
       return <Square value={i} />;
     }
